193c2c8ee7a17480ae506612fdb6948670ee1726
andreasbaumgartner <baumgartner.andy@hotmail.ch>
2024-04-28 12:52:15 +0200

    add plugin

---
+++ light_venv_switch.plugin.zsh
@@ -0,0 +1,27 @@
+# automaticly activate venv if exist and prompt a message
+function sv() {
+  builtin cd "$@"
+  local message_activ="👉 Virtualenv activated 🚀"
+  local message_deactiv="👉 Virtualenv deactivated 🤓"
+  local message=true
+
+  # activate
+  if [[ -z "$VIRTUAL_ENV" ]] ; then
+      if [[ -d ./venv ]] ; then
+        source ./venv/bin/activate
+        if [[ $message == true ]] ; then
+          echo $message_activ
+        fi
+      fi
+
+  # deactive
+  else
+      parentdir="$(dirname "$VIRTUAL_ENV")"
+      if [[ "$PWD"/ != "$parentdir"/* ]] ; then
+        deactivate
+        if [[ $message == true ]] ; then
+          echo $message_deactiv
+        fi
+      fi
+  fi
+}
